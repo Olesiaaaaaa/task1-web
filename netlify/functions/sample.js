514dd7c64b98babab5ecfd29b862d4a0bc813a45
Olesiaaaaaa <155964812+Olesiaaaaaa@users.noreply.github.com>
2026-03-24 13:10:01 +0300

    Create sample.js

---
+++ netlify/functions/sample.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+  const functionCode = `function task(x) {
+  return x * this * this;
+}`;
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/plain; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: functionCode
+  };
+};
